601401ad51f39710e5bc59e524f8dbefbaabac75
Daniel Ogbansiegbe <danogbans@gmail.com>
2024-10-16 11:29:39 +0200

    Update urls.py

--- django_pro_blog/urls.py
+++ django_pro_blog/urls.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from django.conf.urls.static import s
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.sitemaps.views import sitemap
